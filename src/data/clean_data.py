1e480ba6a884d2953f0cbfa12c3e1ffdd2882014
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-30 18:58:32 -0500

    Update clean_data.py

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -14,69 +14,57 @@ def clean_data():
     """
     #raise NotImplementedError("Implementar esta función")
     
-    import os 
     import pandas as pd
-    
-    
+    import os
 
-    def read_files(path_origin):
-        correct_prices = pd.DataFrame()
-        files_csv = csv_file(path_origin)
-        for filename in files_csv:
-            if filename.split('.')[-1] == 'csv':
-                data = pd.read_csv(path_origin + filename, index_col=None, header=0)
-                correct_prices = pd.concat(objs=[correct_prices, data], axis=0, ignore_index=False)
-        return correct_prices
+    archivos = ["data_lake/raw/" + f for f in os.listdir("data_lake/raw") if f.endswith(".csv")]
 
-    def csv_file(path_origin):
-        files_csv = os.listdir(path_origin)
-        return files_csv
+    CSV = []
 
-    def created_format(correct_prices):
-        correct_prices['Fecha'] = correct_prices['Fecha'].apply(lambda x: str(x))
-        correct_prices['Fecha'] = correct_prices['Fecha'].apply(lambda x: x[:10])
-        correct_prices = correct_prices[correct_prices['Fecha'].notnull()]
-        return correct_prices
+    for archivo in archivos:
+        data = pd.read_csv(archivo)
+        data = data.fillna(method="bfill", axis=1)
 
-    def create_df():
-        df_correct_prices = pd.DataFrame()
-        df_correct_prices['Fecha'] = None
-        df_correct_prices['Hora'] = None
-        df_correct_prices['Precio'] = None
+        data = pd.melt(
+            data,
+            id_vars=["Fecha"],
+            value_vars=[
+                "H00",
+                "H01",
+                "H02",
+                "H03",
+                "H04",
+                "H05",
+                "H06",
+                "H07",
+                "H08",
+                "H09",
+                "H10",
+                "H11",
+                "H12",
+                "H13",
+                "H14",
+                "H15",
+                "H16",
+                "H17",
+                "H18",
+                "H19",
+                "H20",
+                "H21",
+                "H22",
+                "H23",
+            ],
+        )
+        data.columns = ["Fecha", "Hora", "Precio"]
+        data = data.sort_values(by=["Fecha", "Hora"])
 
-        df_correct_prices_backup = pd.DataFrame()
-        df_correct_prices_backup['Fecha'] = None
-        df_correct_prices_backup['Hora'] = None
-        df_correct_prices_backup['Precio'] = None
-        return df_correct_prices, df_correct_prices_backup
+        CSV.append(data)
 
-    def make_correct_prices(correct_prices, df_correct_prices, df_correct_prices_backup):
-        for hora in range(0,24):
-            hora_str = str(hora)
+    CSVMerged = pd.concat(CSV, ignore_index=True)
+    CSVMerged.to_csv("data_lake/cleansed/precios-horarios.csv", index=False)
 
-            df_correct_prices_backup['Fecha'] = correct_prices['Fecha']
-            df_correct_prices_backup['Hora'] = hora_str
-            df_correct_prices_backup['Precio'] = correct_prices[hora_str]
-
-            df_correct_prices = pd.concat(objs=[df_correct_prices, df_correct_prices_backup], axis=0, ignore_index=False)
-        return df_correct_prices
-
-    def save_correct_prices(df_correct_prices, path_final):
-        df_correct_prices.to_csv(path_final + 'precios-horarios.csv', index=None)
-
-    path_origin = './data_lake/raw/'
-    path_final = './data_lake/cleansed/'
-    correct_prices = read_files(path_origin)
-    correct_prices = created_format(correct_prices)
-    df_correct_prices, df_correct_prices_backup = create_df()
-    df_correct_prices = make_correct_prices(correct_prices, df_correct_prices, df_correct_prices_backup)
-    save_correct_prices(df_correct_prices, path_final)
-    
 
 if __name__ == "__main__":
-
-    clean_data()
     import doctest
 
     doctest.testmod()
-
