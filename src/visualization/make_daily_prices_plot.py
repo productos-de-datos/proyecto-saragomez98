b568b47f97b8a6242e24accd67dab3319cb15f96
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-30 19:27:45 -0500

    Update make_daily_prices_plot.py

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -7,10 +7,29 @@ def make_daily_prices_plot():
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
 
     """
-    raise NotImplementedError("Implementar esta función")
+   # raise NotImplementedError("Implementar esta función")
+import pandas as pd
+    import os
+
+    
+    os.chdir("./")
+    prices_df = pd.read_csv("data_lake/business/precios-diarios.csv")
+
+    
+    figura = prices_df.plot(
+        kind="line",
+        x="Fecha",
+        y="Precio",
+        title="Precio Promedio Histórico Diario",
+        grid=True,
+        figsize=(10, 5),
+    ).get_figure()
+
+    figura.savefig("data_lake/business/reports/figures/daily_prices.png")
 
 
 if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
+
