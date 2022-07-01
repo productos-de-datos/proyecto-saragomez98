2f0326300f0a287744a100f417fc0137ef69f9e2
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-30 19:40:43 -0500

    Update make_forecasts.py

--- src/models/make_forecasts.py
+++ src/models/make_forecasts.py
@@ -1,11 +1,3 @@
-import pickle
-import os
-import pandas as pd
-import subprocess
-
-subprocess.call(["pip", "install", "skforecast"])
-
-from skforecast.model_selection import backtesting_forecaster
 def make_forecasts():
     """Construya los pronosticos con el modelo entrenado final.
 
@@ -20,44 +12,9 @@ def make_forecasts():
 
 
     """
-    #raise NotImplementedError("Implementar esta función")
+    raise NotImplementedError("Implementar esta función")
 
-    modelo = cargar_modelo()
-
-    datos = pd.read_csv("data_lake/business/features/precios-diarios.csv")
-    datos["Fecha"] = pd.to_datetime(datos["Fecha"], format="%Y-%m-%d")
-    datos = datos.set_index("Fecha")
-    datos = datos.asfreq("D")
-    datos = datos.sort_index()
-
-    datos_backtest = 365 * 20
-    datos_train = datos[:-datos_backtest]
-    datos_test = datos[-datos_backtest:]
-
-    modelo.fit(y=datos_train["Precio"])
-
-    metrica, predicciones = backtesting_forecaster(
-        forecaster=modelo,
-        y=datos.Precio,
-        initial_train_size=len(datos_train),
-        fixed_train_size=False,
-        steps=1,
-        metric="mean_absolute_error",
-        refit=False,
-        verbose=True,
-    )
-
-    predicciones.join(datos_test["Precio"]).to_csv(
-        "data_lake/business/forecasts/precios-diarios.csv", index=True
-    )
-
-
-def cargar_modelo():
     
-    with open("src/models/precios-diarios.pkl", "rb") as file:
-        modelo = pickle.load(file)
-
-    return modelo
 
 
 if __name__ == "__main__":
