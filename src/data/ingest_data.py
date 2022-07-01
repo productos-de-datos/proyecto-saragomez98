b01534834f5cc921fdc462cc137d7877222556c1
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-30 19:06:03 -0500

    Update ingest_data.py

--- src/data/ingest_data.py
+++ src/data/ingest_data.py
@@ -4,7 +4,6 @@ Módulo de ingestión de datos.
 
 """
 
-
 def ingest_data():
     """Ingeste los datos externos a la capa landing del data lake.
 
@@ -13,9 +12,22 @@ def ingest_data():
     descarga debe realizarse usando únicamente funciones de Python.
 
     """
-    raise NotImplementedError("Implementar esta función")
+#    raise NotImplementedError("Implementar esta función")
 
+import requests
 
+    
+    #Ciclo for para recorrer el numero de años (archivos a descargar) segun su extension 
+    for num in range(1995, 2022):
+        if num in range(2016, 2018):
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xls?raw=true'.format(num)
+            file = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xls'.format(num), 'wb').write(file.content)
+        else:
+            url = 'https://github.com/jdvelasq/datalabs/blob/master/datasets/precio_bolsa_nacional/xls/{}.xlsx?raw=true'.format(num)
+            file = requests.get(url, allow_redirects=True)
+            open('data_lake/landing/{}.xlsx'.format(num), 'wb').write(file.content)
+            
 if __name__ == "__main__":
     import doctest
 
