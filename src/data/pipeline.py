c58943d855bf6c43b0f872c848ced878d183aae7
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-30 19:42:20 -0500

    Update pipeline.py

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -11,3 +11,11 @@ En luigi llame las funciones que ya creo.
 
 
 """
+if __name__ == "__main__":
+
+    raise NotImplementedError("Implementar esta función")
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
