e53a2e46d0240d3c5c562669a00fb9834eaf5efd
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-07-01 18:27:55 -0500

    Update pipeline.py

--- src/data/pipeline.py
+++ src/data/pipeline.py
@@ -14,73 +14,81 @@ En luigi llame las funciones que ya creo.
 import luigi
 from luigi import Task, LocalTarget
 
-
-class import_Data(Task):
-
+#Creamos una clase que llame la funcion de importar datos (ingest_data)
+class ingestar_datos(Task):
     def output(self):
-        return LocalTarget('data_lake/landing/arc.csv')
+        return LocalTarget('data_lake/landing/ingestar_datos_pipeline.txt')
 
     def run(self):
+
         from ingest_data import ingest_data
         with self.output().open('w') as archivos:
-            ingest_data
-    
+            ingest_data()
 
-class transform_data(Task):
-
+#Creamos una clase que llame la funcion para tranformar los datos de xls a csv (transform_data), para
+#esto es necesario previamente tener la clase anterior ingestar_datos()
+class transformar_datos(Task):
     def requires(self):
-        return import_Data()
+        return ingestar_datos()
 
     def output(self):
-        return LocalTarget('data_lake/raw/arc.txt')
+        return LocalTarget('data_lake/raw/transformar_datos_pipeline.txt')
 
     def run(self):
+
         from transform_data import transform_data
         with self.output().open('w') as archivos:
             transform_data()
 
-class make_clean_data(Task):
-
+#Creamos una clase que llame la funcion para limpiar los datos y hacer un solo csv (clean_data), para
+#esto es necesario previamente tener la clase anterior transformar_datos()
+class limpiar_datos(Task):
     def requires(self):
-        return transform_data()
-    
+        return transformar_datos()
+
     def output(self):
-        return LocalTarget('data_lake/cleansed/arc.txt')
+        return LocalTarget('data_lake/cleansed/limpiar_datos_pipeline.txt')
 
     def run(self):
+
         from clean_data import clean_data
         with self.output().open('w') as archivos:
             clean_data()
 
-class make_daily_prices(Task):
-
+#Creamos una clase que llame la funcion computar el precio diario promedio (compute_daily_prices), para
+#esto es necesario previamente tener la clase anterior limpiar_datos()
+class precio_diario(Task):
     def requires(self):
-        return make_clean_data()
+        return limpiar_datos()
 
     def output(self):
-        return LocalTarget('data_lake/business/arc.txt') 
-    
+        return LocalTarget('data_lake/business/precio_diario_pipeline.txt')
+
     def run(self):
+
         from compute_daily_prices import compute_daily_prices
         with self.output().open('w') as archivos:
             compute_daily_prices()
 
-class make_monthly_prices(Task):
-
+#Creamos una clase que llame la funcion computar el precio diario promedio (compute_monthly_prices), para
+#esto es necesario previamente tener la clase anterior precio_diario()
+class precio_mensual(Task):
     def requires(self):
-        return make_daily_prices()
-    
+        return precio_diario()
+
     def output(self):
-        return LocalTarget('data_lake/business/arc.txt')
-    
+        return LocalTarget('data_lake/business/precio_mensual_pipeline.txt')
+
     def run(self):
+
         from compute_monthly_prices import compute_monthly_prices
         with self.output().open('w') as archivos:
             compute_monthly_prices()
 
-if __name__ == "__main__":
-     luigi.run(["make_monthly_prices", "--local-scheduler"])
-    
+
+if __name__ == '__main__':
+    luigi.run(["precio_mensual", "--local-scheduler"])
+
 if __name__ == "__main__":
     import doctest
 
