ee8b6a40adc5efa65860b0e70cba2c4b7e86a481
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-30 19:37:49 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -9,41 +9,33 @@ def transform_data():
     """
    # raise NotImplementedError("Implementar esta función")
 import pandas as pd
+import xlwt
+import os
 
-    #Creamos una lista para el encabezado de los archivos transformados 
-    encabezado = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10', 'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
+landing_path = 'data_lake/landing/'
+raw_path = 'data_lake/raw/'
+files_landing_folder = os.listdir(landing_path)
+files_raw_folder = os.listdir(raw_path)
+
+for files in files_landing_folder:
+    if files.split('.')[-1] == 'xlsx' or files.split('.')[-1] == 'xls':
+        year_transformed = int(files.split('.')[0])
+        if year_transformed in range(1995,2000):
+            num_header = 3
+        if year_transformed in range (2000, 2018):
+            num_header = 2
+        if year_transformed in range (2018, 2022):
+            num_header = 0
     
-    #Transformacion de archivos .xls o .xlsx a .csv segun la cantidad de filas superiores vacias que
-    #es necesario eliminar. Se toma las 25 columnas correspondiente a fecha y 24 horas
-    for num in range(1995, 2022):        
-        if num in range(1995, 2000):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=3)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado       
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num),index=None)
-        elif(num in range(2000, 2016)):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=2)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado             
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num),index=None)
-        elif(num in range(2016, 2018)):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xls'), header=2)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado             
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num), index=None)
-        else:
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=0)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado            
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num), index=None)
-
-
-    #raise NotImplementedError("Implementar esta función")
-
+        file = pd.read_excel(landing_path + files)
+        file = file.iloc[num_header:, 0:25]
+        file.columns = ['Fecha', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
+        file.to_csv(raw_path + str(year_transformed) + '.csv', index=None)
+        
 
 if __name__ == "__main__":
     import doctest
 
-    doctest.testmod()
     transform_data()
 
+    doctest.testmod()
