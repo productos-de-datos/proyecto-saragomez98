65c820d6ce5492419c3b8cc4278e8b37714ca9cb
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-06-30 19:32:16 -0500

    Update compute_monthly_prices.py

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -15,18 +15,23 @@ def compute_monthly_prices():
    # raise NotImplementedError("Implementar esta función")
 
 import pandas as pd
+    import os
 
+    path_origin = 'data_lake/cleansed/precios-horarios.csv'
+    final_path = 'data_lake/business/'
     
-    data = pd.read_csv("data_lake/cleansed/precios-horarios.csv")
-    data["Fecha"] = pd.to_datetime(data["Fecha"])
-
+    day_prices = pd.read_csv('data_lake/cleansed/precios-horarios.csv', index_col=None, header=0)
+    day_prices = day_prices[['Fecha', 'Precio']]
+    day_prices['Fecha'] = pd.to_datetime(day_prices['Fecha'])
+    day_prices['Firstday']= day_prices['Fecha'].to_numpy().astype('datetime64[M]')
+    day_prices = day_prices[['Firstday', 'Precio']]
+    monthly_ave_prices = day_prices.groupby('Firstday', as_index=False).mean({'Precio': 'Precio'})
+    monthly_ave_prices.to_csv( final_path + 'precios-mensuales' + '.csv', index=None, header= True)
     
-    data = data.set_index("Fecha").resample("M")["Precio"].mean()
-
-    
-    data.to_csv("data_lake/business/precios-mensuales.csv", index=True)
 
 if __name__ == "__main__":
+
+    compute_monthly_prices()
     import doctest
 
     doctest.testmod()
