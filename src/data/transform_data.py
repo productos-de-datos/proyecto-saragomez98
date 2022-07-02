0add53b9cc11f1f61db84f64cf52d5cd0f44c73d
saragomez98 <102761696+saragomez98@users.noreply.github.com>
2022-07-01 19:16:14 -0500

    Update transform_data.py

--- src/data/transform_data.py
+++ src/data/transform_data.py
@@ -5,44 +5,42 @@ def transform_data():
     tiene como columnas la fecha en formato YYYY-MM-DD y las horas H00, ...,
     H23.
     """
-
     import pandas as pd
-    import os
-    import openpyxl
-
-    #Creamos una lista para el encabezado de los archivos transformados 
-    encabezado = ['Fecha', 'H00', 'H01', 'H02', 'H03', 'H04', 'H05', 'H06', 'H07', 'H08', 'H09', 'H10', 'H11', 'H12', 'H13', 'H14', 'H15', 'H16', 'H17', 'H18', 'H19', 'H20', 'H21', 'H22', 'H23']
-    
-    #Transformacion de archivos .xls o .xlsx a .csv segun la cantidad de filas superiores vacias que
-    #es necesario eliminar. Se toma las 25 columnas correspondiente a fecha y 24 horas
-    for num in range(1995, 2022):        
-        if num in range(1995, 2000):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=3)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado       
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num),index=None)
-        elif(num in range(2000, 2016)):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=2)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado             
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num),index=None)
-        elif(num in range(2016, 2018)):
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xls'), header=2)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado             
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num), index=None)
+    for file in range(1995, 2022):
+        if file in range(1995, 2000):
+            read_file = pd.read_excel(
+                'data_lake/landing/{}.xlsx'.format(file), header=3)
+            read_file.to_csv(
+                'data_lake/raw/{}.csv'.format(file), index=None)
+        elif file in range(2000, 2018):
+            if file in [2016, 2017]:
+                read_file = pd.read_excel(
+                    'data_lake/landing/{}.xls'.format(file), header=2)
+                read_file.to_csv(
+                    'data_lake/raw/{}.csv'.format(file), index=None)
+            else:
+                read_file = pd.read_excel(
+                    'data_lake/landing/{}.xlsx'.format(file), header=2)
+                read_file.to_csv(
+                    'data_lake/raw/{}.csv'.format(file), index=None)
         else:
-            data_csv = pd.read_excel('data_lake/landing/{}.{}'.format(num, 'xlsx'), header=0)
-            data_csv = data_csv.iloc[:, 0:25]
-            data_csv.columns = encabezado            
-            data_csv.to_csv('data_lake/raw/{}.csv'.format(num), index=None)
-
-
-    #raise NotImplementedError("Implementar esta función")
-
+            read_file = pd.read_excel(
+                'data_lake/landing/{}.xlsx'.format(file), header=0)
+            read_file.to_csv(
+                'data_lake/raw/{}.csv'.format(file), index=None)
+#raise NotImplementedError("Implementar esta función")
+def test_date_validation():
+    """
+    Testing that the date is in the column that should be
+    """
+    import pandas as pd
+    for file in range(1995, 2022):
+        read_file = pd.read_csv(
+                'data_lake/raw/{}.csv'.format(file))
+        assert ["Fecha"] == [read_file.columns.values[0]]
 
 if __name__ == "__main__":
     import doctest
-
-    doctest.testmod()
     transform_data()
+    doctest.testmod()
+    
